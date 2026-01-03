dde58004a1845593eafc4a57e5df9951d207c010
mouraus <g.gmoura@outlook.com>
2026-01-03 08:28:21 -0300

    teste versão

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,3 @@
-// Entry point principal da aplicação
-// Este arquivo inicia o servidor Express
-
 import "./app";
 
-console.log("Aplicacao iniciada com sucesso!");
+console.log("Aplicacao iniciada com sucesso !");
